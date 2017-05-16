ea2d872421b88702564639a988ee3568c5761c25
olgakravchenko <netalin.dm@gmail.com>
2017-05-16 12:05:39 +0300

    added reverse mtf, removed useless vard from non-reverse mtf

--- mtf.py
+++ mtf.py
@@ -1,22 +1,22 @@
-def encode(string, alphabet):
-    n = len(alphabet)
+def encode(string, a):
+
+    alphabet = list(a)
     occ = {}  # dict w frequencies of every letter's occurrence
-    s = float(len(string))
     ans = []
 
     for ltr in string:
 
         i = alphabet.index(ltr)
-
         if i in occ:
             occ[i] += 1
         else:
             occ[i] = 1
 
         ans.append(i)
-
         alphabet.pop(i)
-
         alphabet.insert(0, ltr)
 
-    return ans, alphabet, occ
+    return ans, occ
+
+
+
